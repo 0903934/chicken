6efcfecfff91572c2cbbbf0384880a541378ddf3
Stuart Cossar <stucos@hotmail.com>
2018-03-13 11:18:57 +0000

    Update db.php

--- db.php
+++ db.php
@@ -1,4 +1,4 @@
-<?php
+/**<?php
 
 /**
  * Created by PhpStorm.
@@ -18,4 +18,41 @@ $conn = mysqli_init();
 mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306);
 if (mysqli_connect_errno($conn)) {
     die('Failed to connect to MySQL: '.mysqli_connect_error());
+}
+*/
+
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1611866
+ * Date: 07/03/2017
+ * Time: 15:50
+ *
+ * Azure db connection script
+ */
+/** Method for connecting the the Database. returns the connection if successful or false if not
+ * @return bool|mysqli: returns the DB link if successful of false if not
+ */
+function dbConnect()
+{
+    $connectstr_dbhost = '';
+    $connectstr_dbname = '';
+    $connectstr_dbusername = '';
+    $connectstr_dbpassword = '';
+    foreach ($_SERVER as $key => $value) {
+        if (strpos($key, "MYSQLCONNSTR_localdb") !== 0) {
+            continue;
+        }
+        $connectstr_dbhost = preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $value);
+        $connectstr_dbname = preg_replace("/^.*Database=(.+?);.*$/", "\\1", $value);
+        $connectstr_dbusername = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
+        $connectstr_dbpassword = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
+    }
+    $link = mysqli_connect($connectstr_dbhost, $connectstr_dbusername, $connectstr_dbpassword, $connectstr_dbname);
+    if (!$link) {
+        // return boolean false for the failed connection
+        return false;
+    }
+    // return the DB connection
+    return $link;
 }
