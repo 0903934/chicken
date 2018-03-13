4b38cbc0a55a0d47257cad7191d1c8031498b739
Stuart Cossar <stucos@hotmail.com>
2018-03-13 11:19:44 +0000

    Update db.php

--- db.php
+++ db.php
@@ -1,4 +1,4 @@
-/**<?php
+<?php
 
 /**
  * Created by PhpStorm.
@@ -8,20 +8,18 @@
  */
 
 //THERE IS NEED TO REPLACE THIS PORTION WITH CODE FROM SERVER TO GIVE ACCESS TO DATABASE.
-$host = '127.0.0.1';
-$username = 'root';
-$password = '';
-$db_name = 'predatordetection';
+//$host = '127.0.0.1';
+//$username = 'root';
+//$password = '';
+//$db_name = 'predatordetection';
 
 //Establishes the connection
-$conn = mysqli_init();
-mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306);
-if (mysqli_connect_errno($conn)) {
-    die('Failed to connect to MySQL: '.mysqli_connect_error());
-}
-*/
+//$conn = mysqli_init();
+//mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306);
+//if (mysqli_connect_errno($conn)) {
+//    die('Failed to connect to MySQL: '.mysqli_connect_error());
+//}
 
-<?php
 /**
  * Created by PhpStorm.
  * User: 1611866
