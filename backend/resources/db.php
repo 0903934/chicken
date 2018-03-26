393ad0aaf643f477baa0b92db8a6804b06f7e50a
Temple <okosun.temple@gmail.com>
2018-03-26 13:42:46 +0100

    preparing-for-viewsCreation-UserReportViewBegins5

--- backend/resources/db.php
+++ backend/resources/db.php
@@ -1,35 +1,18 @@
 <?php
-
 /**
  * Created by PhpStorm.
  * User: Temple
  * Date: 09-Mar-18
  * Time: 5:18 PM
  */
-
-// set db mane
+//THERE IS NEED TO REPLACE THIS PORTION WITH CODE FROM SERVER TO GIVE ACCESS TO DATABASE.
+$host = '127.0.0.1';
+$username = 'root';
+$password = '';
 $db_name = 'predatordetection';
-
-// set empty variables for the connection
-$connectstr_dbhost = '';
-$connectstr_dbname = '';
-$connectstr_dbusername = '';
-$connectstr_dbpassword = '';
-
-// get log in details from azure
-foreach ($_SERVER as $key => $value) {
-    if (strpos($key, "MYSQLCONNSTR_localdb") !== 0) {
-        continue;
-    }
-    $connectstr_dbhost = preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $value);
-    $connectstr_dbname = preg_replace("/^.*Database=(.+?);.*$/", "\\1", $value);
-    $connectstr_dbusername = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
-    $connectstr_dbpassword = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
-}
-
 //Establishes the connection
 $conn = mysqli_init();
-mysqli_real_connect($conn, $connectstr_dbhost, $connectstr_dbusername, $connectstr_dbpassword, $db_name, 3306); 
+mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306);
 if (mysqli_connect_errno($conn)) {
     die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
