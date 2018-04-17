86cd32cd26d91017e24c822c81baa282cf9249c9
Temple <okosun.temple@gmail.com>
2018-04-17 13:46:53 +0100

    registration-correction7

--- backend/logic/AddAUser.php
+++ backend/logic/AddAUser.php
@@ -58,12 +58,8 @@ if (isset($_REQUEST['username'])) {
 
         $queryNewUser = "INSERT INTO users (UserName, Password, FirstName, LastName, Email, UserType, emailactivation, token, registrationdate) 
                           VALUES ('$user_name', '$user_password', '$firstname', '$lastname', '$email', '$usertype', '$EmailActivation', '$token', '$DateTime')";
-        $resultNewUser = mysqli_query($conn, $queryNewUser) or die( 'Error: ' . mysqli_error($conn));
-        if (!mysqli_query($conn, $resultNewUser))
-        {
-            echo("Error description: " . mysqli_error($conn));
-        }
-        elseif ($resultNewUser) {
+        $resultNewUser = mysqli_query($conn, $queryNewUser);
+        if ($resultNewUser) {
                 //Send an Activation Email. We will use PHP Mailer to avoid our mails been filtered off as Spam.
                 include_once "../../backend/resources/PHPMailer/PHPMailer.php";
                 include_once "../../backend/resources/PHPMailer/Exception.php";
@@ -129,7 +125,7 @@ if (isset($_REQUEST['username'])) {
                 }
             }
     else {
-        echo mysqli_connect_error();
+        echo die( 'Error: ' . mysqli_error($conn));
         //Redirect_to('../../index.html');
     }
 }
